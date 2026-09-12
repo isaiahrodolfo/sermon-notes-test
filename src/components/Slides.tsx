21403916b97d4751590864e4a448fcb287f77050
isaiahrodolfo <planetquack@gmail.com>
2026-09-11 21:20:00 -0700

    added skeleton slides and text editor elements

---
+++ src/components/Slides.tsx
@@ -0,0 +1,5 @@
+import { Text } from "react-native";
+
+export default function Slides() {
+  return <Text>Slides</Text>;
+}
---
+++ src/components/TextEditor.tsx
@@ -0,0 +1,5 @@
+import { Text } from "react-native";
+
+export default function TextEditor() {
+  return <Text>Text Editor</Text>;
+}
