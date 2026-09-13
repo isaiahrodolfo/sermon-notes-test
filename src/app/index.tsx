878c4f4c943ab222ab5e05fa278631c7aa875dee
isaiahrodolfo <planetquack@gmail.com>
2026-09-12 20:45:31 -0700

    single page and zoom

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,71 +1,86 @@
-/**
- * Copyright (c) 2017-present, Wonday (@wonday.org)
- * All rights reserved.
- *
- * This source code is licensed under the MIT-style license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
 import React from "react";
 import { Button, Dimensions, StyleSheet, View } from "react-native";
 import Pdf from "react-native-pdf";
 
 export default function PDFExample() {
   const [currentPage, setCurrentPage] = React.useState(1);
+  const [pdfAspectRatio, setPdfAspectRatio] = React.useState<number | null>(
+    null,
+  );
+
   const pdfRef = React.useRef<any>(null);
 
+  const screenWidth = Dimensions.get("window").width;
+  const pdfWidth = screenWidth - 20;
+
+  const pdfHeight = pdfAspectRatio ? pdfWidth / pdfAspectRatio : pdfWidth;
+
   const source = {
-    uri: "https://github.com/douglasjunior/react-native-pdf-renderer/raw/refs/heads/main/Sample/A17_FlightPlan.pdf",
+    uri: "https://github.com/ralfstuckert/pdfbox-layout/raw/master/examples/letter.pdf",
     cache: true,
   };
 
   const handleChangePage = (delta: number) => () => {
-    if (pdfRef.current) {
-      const newPage = currentPage + delta;
-      if (newPage > 0) {
-        pdfRef.current.setPage(newPage);
-        setCurrentPage(newPage);
-      }
+    const newPage = currentPage + delta;
+
+    if (newPage > 0) {
+      pdfRef.current?.setPage(newPage);
+      setCurrentPage(newPage);
     }
   };
 
   return (
-    <View style={styles.container}>
-      <Button title="Next Page" onPress={handleChangePage(1)} />
-      <Button title="Previous Page" onPress={handleChangePage(-1)} />
-      <Pdf
-        ref={pdfRef}
-        source={source}
-        onLoadComplete={(numberOfPages, filePath) => {
-          console.log(`Number of pages: ${numberOfPages}`);
+    <View style={styles.screen}>
+      <View
+        style={{
+          width: pdfWidth,
+          height: pdfHeight,
         }}
-        onPageChanged={(page, numberOfPages) => {
-          console.log(`Current page: ${page}`);
-        }}
-        onError={(error) => {
-          console.log(error);
-        }}
-        onPressLink={(uri) => {
-          console.log(`Link pressed: ${uri}`);
-        }}
-        singlePage={true}
-        scrollEnabled={false}
-        style={styles.pdf}
-      />
+      >
+        <Pdf
+          ref={pdfRef}
+          source={source}
+          onLoadComplete={(numberOfPages, filePath, { width, height }) => {
+            console.log(`Number of pages: ${numberOfPages}`);
+            console.log(`PDF size: ${width} x ${height}`);
+
+            setPdfAspectRatio(width / height);
+          }}
+          onPageChanged={(page, numberOfPages) => {
+            setCurrentPage(page);
+          }}
+          onError={(error) => {
+            console.log(error);
+          }}
+          onPressLink={(uri) => {
+            console.log(`Link pressed: ${uri}`);
+          }}
+          style={styles.pdf}
+        />
+      </View>
+
+      <View style={styles.buttons}>
+        <Button title="Previous Page" onPress={handleChangePage(-1)} />
+        <Button title="Next Page" onPress={handleChangePage(1)} />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  screen: {
     flex: 1,
-    justifyContent: "flex-start",
     alignItems: "center",
     marginTop: 25,
   },
+
   pdf: {
     flex: 1,
-    width: Dimensions.get("window").width,
-    height: Dimensions.get("window").height,
+  },
+
+  buttons: {
+    flexDirection: "row",
+    gap: 10,
+    marginTop: 10,
   },
 });
